946709e53fa1201ffcefab635ffcbcbb55f808a7
MDev <46203814+Matteo-Dev@users.noreply.github.com>
2021-12-12 23:04:01 +0100

    Arrowing system works (needs refactoring!)

--- dist/base/keyHandler.js
+++ dist/base/keyHandler.js
@@ -1,3 +1,4 @@
+import Component from "../core/component.js";
 class KeyHandler {
     constructor(refComponent) {
         this.refComponent = refComponent;
@@ -8,24 +9,63 @@ class KeyHandler {
     }
     handleUpArrow(e) {
         if (e.key == "ArrowUp") {
-            let selection = document.getSelection();
-            console.log(selection);
+            if (getCursorLine(e.target) <= 1) {
+                e.preventDefault();
+                let prevComp = Component.tedit.collection.prev(this.refComponent);
+                let selection = document.getSelection();
+                let endPos;
+                endPos = (selection === null || selection === void 0 ? void 0 : selection.focusOffset) || 0;
+                prevComp === null || prevComp === void 0 ? void 0 : prevComp.focus(endPos);
+            }
         }
     }
     handleDownArrow(e) {
         if (e.key == "ArrowDown") {
+            console.log(countLines(e.target));
+            if (getCursorLine(e.target) == countLines(e.target)) {
+                e.preventDefault();
+                let nextComp = Component.tedit.collection.next(this.refComponent);
+                let selection = document.getSelection();
+                let endPos;
+                endPos = (selection === null || selection === void 0 ? void 0 : selection.focusOffset) || 0;
+                nextComp === null || nextComp === void 0 ? void 0 : nextComp.focus(endPos);
+            }
         }
     }
 }
-function getLineHeight(element) {
+function createMirror(element) {
     const m = document.createElement("p");
     m.style.width = element.clientWidth.toString();
-    m.style = element.style;
-    m.innerHTML = "h";
-    document.body.appendChild(m);
+    m.style.wordWrap = window.getComputedStyle(element).wordWrap;
+    m.style.whiteSpace = window.getComputedStyle(element).whiteSpace;
+    m.style.padding = element.style.padding;
+    m.style.width = window.getComputedStyle(element).width;
+    m.style.fontFamily = window.getComputedStyle(element).fontFamily;
+    m.style.fontSize = window.getComputedStyle(element).fontSize;
+    m.style.lineHeight = window.getComputedStyle(element).lineHeight;
     m.style.opacity = "0";
     m.style.position = "fixed";
     m.style.zIndex = "-1";
+    return m;
+}
+function getLineHeight(element) {
+    const m = createMirror(element);
+    m.innerHTML = "h";
+    document.body.appendChild(m);
+    return m.clientHeight - (+m.style.paddingTop.slice(0, 1) + +m.style.paddingBottom.slice(0, 1));
+}
+function countLines(element) {
+    const lineHeight = getLineHeight(element);
+    return (element.clientHeight - (+element.style.paddingTop.slice(0, 1) + +element.style.paddingBottom.slice(0, 1))) / lineHeight;
+}
+function getCursorLine(element) {
+    var _a;
+    const lineHeight = getLineHeight(element);
+    const selection = document.getSelection();
+    const m = createMirror(element);
+    m.innerHTML = ((_a = element.textContent) === null || _a === void 0 ? void 0 : _a.slice(0, selection === null || selection === void 0 ? void 0 : selection.anchorOffset)) || "";
+    document.body.appendChild(m);
+    return (m.clientHeight - (+m.style.paddingTop.slice(0, 1) + +m.style.paddingBottom.slice(0, 1))) / lineHeight;
 }
 export default KeyHandler;
 //# sourceMappingURL=keyHandler.js.map
--- src/base/keyHandler.ts
+++ src/base/keyHandler.ts
@@ -18,41 +18,79 @@ class KeyHandler {
 
     public handleUpArrow(e:KeyboardEvent){
         if (e.key == "ArrowUp"){
-            // e.preventDefault();
-            // let prevComp = Component.tedit.collection.prev(this.refComponent);
-            // let selection = document.getSelection();
-            // let endPos;
-            // endPos = selection?.focusOffset || 0;
-            // prevComp?.focus(endPos);
-
-            let selection = document.getSelection();
-            console.log(selection);
+            if(getCursorLine(e.target as HTMLElement) <= 1){
+                e.preventDefault();
+                let prevComp = Component.tedit.collection.prev(this.refComponent);
+                let selection = document.getSelection();
+                let endPos;
+                endPos = selection?.focusOffset || 0;
+                prevComp?.focus(endPos);
+            } 
         }
     }
 
     public handleDownArrow(e:KeyboardEvent){
         if (e.key == "ArrowDown"){
-            // e.preventDefault();
-            // let nextComp = Component.tedit.collection.next(this.refComponent);
-            // let selection = document.getSelection();
-            // let endPos;
-            // endPos = selection?.focusOffset || 0;
+            console.log(countLines(e.target as HTMLElement));
+            if(getCursorLine(e.target as HTMLElement) == countLines(e.target as HTMLElement)){
+                e.preventDefault();
+                let nextComp = Component.tedit.collection.next(this.refComponent);
+                let selection = document.getSelection();
+                let endPos;
+                endPos = selection?.focusOffset || 0;
 
-            // nextComp?.focus(endPos);
+                nextComp?.focus(endPos);
+            } 
         }
     }
 }
 
 // helpers
-function getLineHeight(element: HTMLElement){
+
+// ! refactor: using the same mirror element for performance reasons
+function createMirror(element:HTMLElement){
     const m = document.createElement("p");
     m.style.width = element.clientWidth.toString();
-    m.style = element.style;
-    m.innerHTML = "h";
-    document.body.appendChild(m);
+    m.style.wordWrap = window.getComputedStyle(element).wordWrap;
+    m.style.whiteSpace = window.getComputedStyle(element).whiteSpace;
+    m.style.padding = element.style.padding;
+    m.style.width = window.getComputedStyle(element).width;
+    m.style.fontFamily = window.getComputedStyle(element).fontFamily;
+    m.style.fontSize = window.getComputedStyle(element).fontSize;
+    m.style.lineHeight = window.getComputedStyle(element).lineHeight;
     m.style.opacity = "0";
     m.style.position = "fixed";
     m.style.zIndex = "-1";
+
+    return m;
+}
+
+function getLineHeight(element: HTMLElement){
+    const m = createMirror(element);
+
+    m.innerHTML = "h";
+    document.body.appendChild(m);
+
+    // ! has to be replaced with a regex
+    return m.clientHeight - (+m.style.paddingTop.slice(0,1) + +m.style.paddingBottom.slice(0,1));
+}
+
+function countLines(element: HTMLElement){
+    const lineHeight = getLineHeight(element);
+
+    return (element.clientHeight - (+element.style.paddingTop.slice(0,1) + +element.style.paddingBottom.slice(0,1))) / lineHeight;
+}
+
+function getCursorLine(element: HTMLElement){
+    const lineHeight = getLineHeight(element);
+
+    const selection = document.getSelection();
+  
+    const m = createMirror(element);
+    m.innerHTML = element.textContent?.slice(0,selection?.anchorOffset) || "";
+    document.body.appendChild(m);
+
+    return (m.clientHeight - (+m.style.paddingTop.slice(0,1) + +m.style.paddingBottom.slice(0,1))) / lineHeight;
 }
 
 export default KeyHandler;
