72d285a2ae05aa38f44d342f9a75251578f196f9
MDev <46203814+Matteo-Dev@users.noreply.github.com>
2021-12-12 17:30:09 +0100

    Started implementation of arrowing system (rows/lines)

--- dist/base/keyHandler.js
+++ dist/base/keyHandler.js
@@ -1,4 +1,3 @@
-import Component from "../core/component.js";
 class KeyHandler {
     constructor(refComponent) {
         this.refComponent = refComponent;
@@ -9,24 +8,24 @@ class KeyHandler {
     }
     handleUpArrow(e) {
         if (e.key == "ArrowUp") {
-            e.preventDefault();
-            let prevComp = Component.tedit.collection.prev(this.refComponent);
             let selection = document.getSelection();
-            let endPos;
-            endPos = (selection === null || selection === void 0 ? void 0 : selection.focusOffset) || 0;
-            prevComp === null || prevComp === void 0 ? void 0 : prevComp.focus(endPos);
+            console.log(selection);
         }
     }
     handleDownArrow(e) {
         if (e.key == "ArrowDown") {
-            e.preventDefault();
-            let nextComp = Component.tedit.collection.next(this.refComponent);
-            let selection = document.getSelection();
-            let endPos;
-            endPos = (selection === null || selection === void 0 ? void 0 : selection.focusOffset) || 0;
-            nextComp === null || nextComp === void 0 ? void 0 : nextComp.focus(endPos);
         }
     }
+}
+function getLineHeight(element) {
+    const m = document.createElement("p");
+    m.style.width = element.clientWidth.toString();
+    m.style = element.style;
+    m.innerHTML = "h";
+    document.body.appendChild(m);
+    m.style.opacity = "0";
+    m.style.position = "fixed";
+    m.style.zIndex = "-1";
 }
 export default KeyHandler;
 //# sourceMappingURL=keyHandler.js.map
--- src/base/keyHandler.ts
+++ src/base/keyHandler.ts
@@ -18,27 +18,41 @@ class KeyHandler {
 
     public handleUpArrow(e:KeyboardEvent){
         if (e.key == "ArrowUp"){
-            e.preventDefault();
-            let prevComp = Component.tedit.collection.prev(this.refComponent);
-            let selection = document.getSelection();
-            let endPos;
-            endPos = selection?.focusOffset || 0;
+            // e.preventDefault();
+            // let prevComp = Component.tedit.collection.prev(this.refComponent);
+            // let selection = document.getSelection();
+            // let endPos;
+            // endPos = selection?.focusOffset || 0;
+            // prevComp?.focus(endPos);
 
-            prevComp?.focus(endPos);
+            let selection = document.getSelection();
+            console.log(selection);
         }
     }
 
     public handleDownArrow(e:KeyboardEvent){
         if (e.key == "ArrowDown"){
-            e.preventDefault();
-            let nextComp = Component.tedit.collection.next(this.refComponent);
-            let selection = document.getSelection();
-            let endPos;
-            endPos = selection?.focusOffset || 0;
+            // e.preventDefault();
+            // let nextComp = Component.tedit.collection.next(this.refComponent);
+            // let selection = document.getSelection();
+            // let endPos;
+            // endPos = selection?.focusOffset || 0;
 
-            nextComp?.focus(endPos);
+            // nextComp?.focus(endPos);
         }
     }
+}
+
+// helpers
+function getLineHeight(element: HTMLElement){
+    const m = document.createElement("p");
+    m.style.width = element.clientWidth.toString();
+    m.style = element.style;
+    m.innerHTML = "h";
+    document.body.appendChild(m);
+    m.style.opacity = "0";
+    m.style.position = "fixed";
+    m.style.zIndex = "-1";
 }
 
 export default KeyHandler;
